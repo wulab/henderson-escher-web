b29242278913beb46da12161edfa03600e2f647c
Weera Wu <ing@oozou.com>
2021-07-18 04:06:35 +0700

    Import henderson escher codes

--- graphics.scm
+++ graphics.scm
@@ -1,3 +1,22 @@
+(load "vector.scm")
+
+(define *viewport* 'viewport)
+(define *canvas-size* 'canvas-size)
+
+(define (make-viewport name w h)
+  (set! *viewport* (open-viewport name w h))
+  (set! *canvas-size* (cons w h))
+  *viewport*)
+
+(define (line-draw v0 v1)
+  (let ((x0 (xcor v0))
+        (y0 (- (cdr *canvas-size*) (ycor v0)))
+        (x1 (xcor v1))
+        (y1 (- (cdr *canvas-size*) (ycor v1))))
+  (draw-line *viewport* x0 y0 x1 y1)))
+
+;; bridge to the same functions in graphics.js
+
 (define (open-viewport name w h)
   (js-call (js-eval "openViewport") name w h))
 
--- hello-graphics.scm
+++
@@ -1,7 +0,0 @@
-(load "graphics.scm")
-
-(let ((viewport (open-viewport "hello-graphics.scm" 400 300)))
-  (draw-line viewport 100 100 100 200)
-  (draw-line viewport 200 100 200 200)
-  (draw-line viewport 100 150 200 150)
-  (draw-string viewport "E L L O" 210 150))
