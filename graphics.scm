0831e5693840118dff487d53872d3dce7136a6ab
Weera Wu <ing@oozou.com>
2021-07-18 02:44:34 +0700

    Add line drawing function

--- graphics.scm
+++ graphics.scm
@@ -1,2 +1,11 @@
-(define (draw-string str)
-  (js-call (js-eval "drawString") str))
+(define viewport 'viewport)
+
+(define (open-viewport name w h)
+  (set! viewport (js-call (js-eval "openViewport") name w h))
+  viewport)
+
+(define (draw-line vp x0 y0 x1 y1)
+  (js-call (js-eval "drawLine") vp x0 y0 x1 y1))
+
+(define (draw-string vp text x y)
+  (js-call (js-eval "drawString") vp text x y))
--- hello-graphics.scm
+++ hello-graphics.scm
@@ -1,3 +1,7 @@
 (load "graphics.scm")
 
-(draw-string "Hello")
+(open-viewport "hello-graphics.scm" 400 300)
+(draw-line viewport 100 100 100 200)
+(draw-line viewport 200 100 200 200)
+(draw-line viewport 100 150 200 150)
+(draw-string viewport "E L L O" 210 150)
